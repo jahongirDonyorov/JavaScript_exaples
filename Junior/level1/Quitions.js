6f73a069f373ac8696700716423b27b18e697d9f
Jahon <donyorovjahon@gmail.com>
2023-07-17 17:57:24 +0100

    Junior examples

---
+++ Junior/level1/Quitions.js
@@ -0,0 +1,314 @@
+// -=-=-=-=-=- Junior strong Livel
+
+// // No 1
+// // object keylarini toping
+// let obj={
+//   id:1,
+//   name:"WebBrain"
+// }
+
+// // chiqish
+// // value: id,name
+
+// -=-=-=-=-= answer No 1
+
+// ###############
+
+// // No2
+// // object valuelarining raqamga tenglarini toping
+// let obj={
+//     id:1,
+//     name:"WebBrain",
+//     age:18,
+//     hight:1.60,
+// }
+
+// // Chiqish:
+// // id,age,hight
+
+// -=-=-=-=-= answer No 2
+
+// ###############
+
+// No3
+
+// // Object valuelarining boolean tipidagi malumotlrini qaytaring keylari blan biriga
+// let obj={
+//     id:1,
+//     name:"WebBrain",
+//     offline:true,
+//     online:true,
+//     individual:false
+// }
+// // chiqish:
+// // {   offline:true,
+// //     online:true,
+// //     individual:false
+// // }
+
+// -=-=-=-=-= answer No 3
+
+// ###############
+
+// No4
+
+// // fnc ga object va string berilgan. agar object keylari ichida str ingga teng bolgan key bolsa shu key va valueni chiqazing
+// // code function ichida yozilishi kerak
+// let obj={
+//     id:1,
+//     name:"WebBrain",
+//     offline:true,
+//     online:true,
+//     individual:false
+// }
+
+// // chiqish
+// // name:"WebBrain"
+
+// -=-=-=-=-= answer No 4
+
+// ###############
+
+// No5
+
+// // object va string berilgan.  string bor bolgan object valuelarining chiqaring
+// let obj={
+//   id:1,
+//   name:"atirgul",
+//   color:"qizil",
+//   count:10,
+//   price:"10$"
+// }
+
+// -=-=-=-=-= answer No 5
+
+// ###############
+
+// // No6
+// // // object ichida objectlar berilgan. ichki objectlarning ichiga age degan key berilgan. barcha ichki keylardagi age lar yigindisini toping
+// let person = {
+//     id: 1,
+//     name: 'Odil',
+//     age:78,
+//     child: {
+//         id: 1,
+//         name: 'Ali',
+//         age:48,
+//         child: {
+//             id: 1,
+//             name: 'Umar',
+//             age:20
+//         }
+//     }
+// }
+
+// chiqish
+// value: 146
+
+// -=-=-=-=-= answer No 6
+
+// ###############
+
+// No7
+// // array ichida object berilgan. job berilgan objectlarni qaytaring
+// let arr=[
+//     {id:1,name:'Usmon',job:'developer'},
+//     {id:2,name:'Usmon',job:'developer'},
+//     {id:3,name:'Usmon'},
+//     {id:4,name:'Usmon',job:'developer'},
+//     {id:5,name:'Usmon'}
+// ]
+
+// -=-=-=-=-= answer No 7
+
+// ###############
+
+// No8
+// // objectdan clone oling birinchi objni o'zgartirsa ikkinchisi malumotlari ozgarmasligi kk
+// let obj={
+//   id:1,name:'Usmon',job:'developer'
+// }
+
+// -=-=-=-=-= answer No 8
+
+// ###############
+
+// // No9
+// // objectga yangi qiymat qo'shuvchi funksiya yozing
+
+// add(obj,key,value)
+// add(obj,'address','toshkent')
+
+// -=-=-=-=-= answer No 9
+
+// ###############
+
+// No10
+// agar object ichida practice 1 dan katta bolsa 'qabul qilindi'.aks holda 'ming bor uzur.' deb chiqsin
+// let obj = {
+//     id: 1,
+//     name: 'Usmon',
+//     job: 'developer',
+//     practice:2
+// }
+// // chiqish
+// // qabul qilindi
+
+// -=-=-=-=-= answer No 10
+
+// ###############
+
+// No11
+
+// // Agar object ichida talaba bolsa objectga {kiridit:true} key va value qoshilsin
+// // aks holsa {kiridit:olinmadi} qoshilsin
+
+// let obj = {
+//     id: 1,
+//     name: 'Usmon',
+//     job: 'developer',
+//     practice:1,
+//     talaba:true
+// }
+
+// chiqish
+// obj ga {kiridit:true} qushilsin
+
+// -=-=-=-=-= answer No 11
+
+// ###############
+
+// No12
+// /* object ichida tugilgan yil,hozirgi yoshi malumotlari keltirilgan.
+//    agar tug'ilgan yil(year) hozirgi yoshiga tug'ri kelmasa 'xato malumot kiritilgan disin' */
+
+// let obj = {
+//     id: 1,
+//     name: 'Usmon',
+//     age:34,
+//     year:1990
+// }
+
+// // chiqish
+// // 'xato malumot kiritdingiz'
+
+// -=-=-=-=-= answer No 12
+
+// ###############
+
+// No13
+
+/* Talaba o'z malumotlarini toldirmoqda. sizga massiv berilgan. 
+massiv ichida uzbekiston viloyatlari berilgan. agar talaba massiv ichidagi bor viloyatlarni
+tanlasa 'Ruyhatdan muvoffaqqiyatli uttingiz' aks holda
+'bizning filallarimiz faqat shu viloyatlarda bor' degan natija chiqsin. */
+
+// arr = ['Samarqand','Andijon','Namangan','Qarshi','Toshkent']
+
+// -=-=-=-=-= answer No 13
+
+// ###############
+
+// No14
+
+// /* talabalar malumotlari massiv berilgan. massiv objectlardan tashkil topgan.
+//  objectda login parol keltirilgan.funksiyaga login va parol jonatiladi.
+//  agar login parol togri bolsa 'hush kelibsiz' aks holda 'login yoki parol xato degan qiymat chiqazing' */
+
+// let arr=[
+//     {id:3,name:'Usmon',parol:'1231'},
+//     {id:1,name:'Umar',parol:'1241'},
+//     {id:5,name:'Jasur',parol:'3452'},
+//     {id:2,name:'Asmo',parol:'2312'},
+//     {id:4,name:'Salohiddin',parol:'3421'},
+// ]
+
+// // chiqish
+// // logIn('Umar','2113') => xato
+// // logIn('Salohiddin','3421') => hush kelibsiz
+
+// -=-=-=-=-= answer No 14
+
+// ###############
+
+// No15
+// // object ichida objectlar berilgan. ichki objectlar bor yoqligini tekshiring
+// obj={
+//   id:1,
+//   title:'the best',
+//   address:{street:"Farobiy",number:'34'}
+// }
+
+// -=-=-=-=-= answer No 15
+
+// ###############
+
+// No16
+
+// let person = {
+//   id: 1,
+//   name: "Umar",
+//   surname: "Ismoilov",
+//   ielts: 5.5,
+// };
+
+// 1 - shart
+// // agar ielts 5.5 yoki katta bolsa console da qabul qilindi,aks holda qayta topshirish kerak db chiqsin
+// person = {
+//     id: 1,
+//     name: 'Umar',
+//     surname: 'Ismoilov',
+//     ielts: 5.5
+// }
+// // chiqish
+// // qabul qilindi
+
+// -=-=-=-=-= answer No 16. 1-shart
+
+// -=-=-=-=-=-
+
+// 2 - shart
+// // agar ielts 5.5 yoki katta bolsa objectga student:true qoshilsin qiymati bn birga
+// person = {
+//   id: 1,
+//   name: "Umar",
+//   surname: "Ismoilov",
+//   ielts: 5.5,
+// };
+// // chiqish
+// // person = {
+// //     id: 1,
+// //     name: 'Umar',
+// //     surname: 'Ismoilov',
+// //     ielts: 5.5,
+// //     student: true
+// // }
+
+// -=-=-=-=-= answer No 16. 2-shart
+
+// -=-=-=-=-
+
+// 3 - shart
+// // agar student true bolsa unga maxsus key berilsin(math.random+id orqali berilsin)
+// // chiqish
+// // person = { parol: 101 // qushiladi }
+
+// -=-=-=-=-= answer No 16. 2-shart
+
+// -=-=-=-=-=-
+
+// 4 - shart
+// // talaba bazaga kirishi kk shunday funksiya yarating u parol va name ni jonatsin. agar parol bn name bazadagi
+// // parol bn name ga togri kelsa consoleda hush kelibsiz aks holda parol yoki name xato chiqsin
+// person = {
+//     id: 1,
+//     name: 'Umar',
+//     surname: 'Ismoilov',
+//     ielts: 5.5,
+//     student: true,
+//     parol: 101
+// }
+
+// Kirish(101,'Umar') //=> hush kelibsiz
+// Kirish(10,'Umar')  // => parol yoki name xato
+// Kirish(101,'Ali')   // => parol yoki name xato
